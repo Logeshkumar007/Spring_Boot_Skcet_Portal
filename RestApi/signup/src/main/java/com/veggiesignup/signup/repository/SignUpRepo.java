8768b043b20d6e11688dd73e21b427f40487c67d
Logeshkumar <logesh07cbe@gmail.com>
2024-02-29 09:13:35 +0530

    updated

--- RestApi/signup/src/main/java/com/veggiesignup/signup/repository/SignUpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.veggiesignup.signup.repository;
-import com.veggiesignup.signup.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SignUpRepo extends JpaRepository<SignUp,String>{
-    
-}
